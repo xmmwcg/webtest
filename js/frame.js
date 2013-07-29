96b6ec3f7b2c183a0e415165eb1c0e0fa3d514f6
zizotech <xmmwc@qq.com>
2013-07-29 16:27:38 +0800

    xmm

---
+++ js/frame.js
@@ -0,0 +1,12 @@
+var GetTop = function () {
+    var f = arguments.callee, w;
+    if (!f.t) {
+        try {
+            w = window;
+            f.t = w != parent ? (parent.GetTop ? parent.GetTop() : parent.parent.GetTop()) : w;
+        } catch (e) {
+            f.t = parent;
+        }
+    }
+    return f.t;
+}
