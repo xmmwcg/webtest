8808d06c9f8b9f8c4889964bf680567f5ccdc131
zizotech <xmmwc@qq.com>
2013-08-15 14:33:07 +0800

    xmm

--- Project/UI/OverDisplay.js
+++ Project/UI/OverDisplay.js
@@ -2,88 +2,89 @@ KISSY.add("Project/UI/OverDisplay",function(S){
     return function(){
         var OverDisplay = {};
 
-        var OverDisplayElement = S.one("#OverDisplay");
+        var OverDisplayElement = $("#OverDisplay");
         var ParentCon = OverDisplayElement.parent();
+        var Historybar = OverDisplayElement.find(".Bar ul");
         //var CloseBt = OverDisplayElement.one(".close").on("click",function(){
         //    Close();
         //});
-        //var BackBt = OverDisplayElement.one(".back").hide().on("click",function(){
-        //    History();
-        //});
-        var frame = OverDisplayElement.one("#DisplayFrame");
+        var frame = OverDisplayElement.find("#DisplayFrame");
         var histroylist = [];
         var isOpen = false;
 
         var OpenFrame = function(src){
             frame.prop("src",src);
-        }
-        var SetHistory = function(src){
-            if(isOpen){
-                histroylist.push(src);
-            }else{
+        };
+        var SetHistory = function(src,name){
+            Historybar.find("li").removeClass("cur");
+            var hist = {};
+            hist.src = src;
+            hist.name = name;
+            hist.element = $("<li class='cur' onclick='return Display.Back(this);'><span>" + name + "</span></li>");
+            if(!isOpen){
                 histroylist = [];
-                histroylist.push(src);
+                Historybar.empty();
             }
-        }
-        var CountHistory = function(){
-            if(histroylist.length > 1){
-                BackBt.show();
-            }else{
-                BackBt.hide();
-            }
-        }
-        var Open = function(src){
+            histroylist.push(hist);
+            Historybar.append(hist.element);
+        };
+        var Open = function(src,name){
             var PWidth = ParentCon.width();
             SetWidth();
-            SetHistory(src);
-            CountHistory();
+            SetHistory(src,name);
             OpenFrame(src);
             if(!isOpen){
                 OverDisplayElement.show();
-                OverDisplayElement.stop().animate({"left": 0},{duration:0.5,complete:function(){
+                OverDisplayElement.stop().animate({"left": 0},500,function(){
                     isOpen = true;
-                }});
+                });
             }
-        }
-        var History = function(){
-            if(histroylist.length > 1){
-                var PWidth = ParentCon.width();
-                histroylist.splice(histroylist.length - 1);
-                var src = histroylist.slice(histroylist.length - 1);
-                SetWidth();
-                CountHistory();
-                OpenFrame(src);
-            }
-        }
+        };
+        var History = function(self){
+             for(var i = 0 ; i <histroylist.length ; i++ ){
+                if(histroylist[i].element[0] == self){
+                    histroylist.splice(i + 1);
+                    var baritems = Historybar.find("li");
+                    baritems.eq(i).addClass("cur");
+                    OpenFrame(histroylist[i].src);
+                    for(var j = i + 1; j < baritems.length ; j++){
+                        baritems.eq(j).remove();
+                    }
+                    break;
+                }
+             }
+        };
         var Close = function(){
             var PWidth = ParentCon.width();
             histroylist = [];
             SetWidth();
             if(isOpen){
-                OverDisplayElement.stop().animate({"left": PWidth},{duration:0.5,complete:function(){
+                OverDisplayElement.stop().animate({"left": PWidth},500,function(){
                     isOpen = false;
                     OverDisplayElement.hide();
-                }});
+                });
             }
-        }
+        };
         //绑定事件
-        S.one(window).on("resize",function(){
+        $(window).bind("resize",function(){
             SetWidth();
-        })
+        });
         //
         var SetWidth = function(){
             var PWidth = ParentCon.width();
             var OLeft = isOpen ? 0 : PWidth;
             OverDisplayElement.css({"left": OLeft + "px","width": PWidth + "px"});
-        }
-        OverDisplay.Open = function(src){
-            Open(src);
-        }
-        OverDisplay.GetHistory = function(){
-            return histroylist;
-        }
+        };
+        OverDisplay.Open = function(src,name){
+            Open(src,name);
+        };
+        OverDisplay.Back = function(self){
+            if(!$(self).hasClass("cur")){
+                History(self);
+            }
+        };
         return OverDisplay;
     }
 },{
     requires:["Project/UI/OverDisplay/OverDisplay.css"]
-})
+});
